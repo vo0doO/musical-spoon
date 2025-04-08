91b99b5f65470e787e299b4945cefba13ffce6e9
vo0doo <kirsanov.bvt@gmail.com>
2025-04-08 18:20:16 +0300

    feat(auth): added logging

--- apps/auth/src/auth/main.py
+++ apps/auth/src/auth/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from auth.middlewares import log_requests
 from auth.router import router
 
 app = FastAPI(
@@ -7,5 +7,5 @@ app = FastAPI(
     description='A secure authentication service to verify user identity before granting access to the system.',
 )
 
-
+app.middleware('http')(log_requests)
 app.include_router(router)
---
+++ apps/auth/src/auth/middlewares.py
@@ -0,0 +1,35 @@
+from collections.abc import Callable
+from datetime import datetime
+from json import JSONDecodeError
+
+from fastapi import Request, Response
+
+from auth.logger import logger
+
+
+async def log_requests(request: Request, call_next: Callable) -> Response:
+    start_time = datetime.now()
+
+    log_data = {
+        'method': request.method,
+        'url': str(request.url),
+        'headers': dict(request.headers),
+        'query_params': dict(request.query_params),
+        'body': await request.body(),
+        'client': request.client.host if request.client else None,
+    }
+
+    try:
+        response = await call_next(request)
+        log_data['status_code'] = response.status_code
+    except Exception as e:
+        log_data['error'] = str(e)
+        logger.error(log_data)
+        raise e
+
+    end_time = datetime.now()
+    log_data['duration_ms'] = (end_time - start_time).total_seconds() * 1000  # type: ignore
+
+    logger.info(log_data)
+
+    return response
